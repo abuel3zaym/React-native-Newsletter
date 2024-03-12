fc305c9ae576bce4bfc8d8419cf5f0e369fc7b29
abuel3zaym <abdullah.abuelazm@gmail.com>
2024-03-12 02:11:11 +0200

    finished welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,55 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Image, Pressable, Text, StyleSheet } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
-  return <View></View>;
+  return (
+    <View style={welcomestyles.container}>
+      <Image 
+      source={require('../assets/little-lemon-logo.png')}
+      style={welcomestyles.logo} />
+      <Text style={welcomestyles.welcometext}>
+        Little lemon your local Mediterranean Bistro
+      </Text>
+      <Pressable style={welcomestyles.button}>
+        <Text style={welcomestyles.buttontxt}>
+          Newsletter
+        </Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default WelcomeScreen;
+
+const welcomestyles = StyleSheet.create({
+  container:{
+    display:'flex',
+    flex:1
+  },
+  logo:{
+    resizeMode: 'contain',
+    height:300,
+    width:400,
+    alignSelf:'center',
+    marginTop:100
+  },
+  welcometext:{
+    alignSelf:'center',
+    fontSize:32,
+    marginTop:50,
+    fontWeight:"bold"
+  },
+  button:{
+    alignSelf:'center',
+    backgroundColor:'olive',
+    paddingHorizontal:100,
+    paddingVertical:10,
+    borderRadius:8,
+    marginTop:50
+  },
+  buttontxt:{
+    fontSize:20,
+    color:'white'
+  }
+})
